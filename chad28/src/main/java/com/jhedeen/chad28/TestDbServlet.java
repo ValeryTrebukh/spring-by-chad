a870e273d84679f99bfeeeba004031c79076e597
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-12-01 16:53:13 +0200

    Build a Database Web App -1
    Spring MVC and Hibernate Project - Part 1

---
+++ chad28/src/main/java/com/jhedeen/chad28/TestDbServlet.java
@@ -0,0 +1,33 @@
+package com.jhedeen.chad28;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String user = "john";
+        String pass = "john";
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+            PrintWriter out = response.getWriter();
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(jdbcUrl, user, pass);
+            System.out.println("Success");
+
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chad28/src/main/webapp/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Customers List</title>
+</head>
+<body>
+
+List Customers - coming soon...
+</body>
+</html>
