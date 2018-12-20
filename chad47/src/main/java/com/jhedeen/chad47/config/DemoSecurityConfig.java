d31c5a98e050ec7fac25ca8e6650ff6e425ce04c
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-12-20 23:05:49 +0200

    Spring Security - Adding Custom Login Form

--- chad47/src/main/java/com/jhedeen/chad47/config/DemoSecurityConfig.java
+++ chad47/src/main/java/com/jhedeen/chad47/config/DemoSecurityConfig.java
@@ -2,6 +2,7 @@ package com.jhedeen.chad47.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -19,5 +20,17 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(users.username("Bob").password("234").roles("CLIENT"))
                 .withUser(users.username("Tom").password("456").roles("MODERATOR"))
                 .withUser(users.username("Dean").password("258").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/showMyLoginPage")
+                    .loginProcessingUrl("/authenticateUser")
+                    .permitAll();
     }
 }
