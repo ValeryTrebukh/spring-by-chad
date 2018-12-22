6940c7810b82a13a8b1d3eb397855a837b8ee3ac
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-12-23 00:51:34 +0200

    Spring REST - Exception Handling - @ControllerAdvice

--- chad58/src/main/java/com/jhedeen/chad58/rest/StudentRestController.java
+++ chad58/src/main/java/com/jhedeen/chad58/rest/StudentRestController.java
@@ -1,8 +1,6 @@
 package com.jhedeen.chad58.rest;
 
 import com.jhedeen.chad58.entity.Student;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
@@ -35,30 +33,6 @@ public class StudentRestController {
         if(id < 0 || id > students.size()) {
             throw new StudentNotFoundException("Student is nod found for id " + id);
         }
-
         return students.get(id);
-    }
-
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (StudentNotFoundException ex) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException (Exception ex) {
-
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
